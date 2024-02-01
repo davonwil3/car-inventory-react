c800b2539c2df27e6a50f2f6419fb2b92fda004d
Davon Wilson <davontech3@gmail.com>
2024-01-31 23:52:14 -0500

    fixed import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SignIn from './singnin';
+import SignIn from './signin';
 import Signup from './signup';
 import Dashboard from './dashboard';
 import ProtectedRoute from './protectedroute';
